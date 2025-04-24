2d8fda6d803c3e3b7dcfcf0f23cc68b506444d8c
aoifem01 <aoife.mccarthy@phs.scot>
2025-04-24 19:38:53 +0100

    adding initial housekeeping

---
+++ code/0_housekeeping.R
@@ -0,0 +1,10 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 1_process_extracts.R
+# Aoife McCarthy
+# April 2025
+# 
+# Creates all housekeeping variables to be used throughout the main scripts
+# 
+# Written/run on Posit WB
+# R version 4.1.2
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
